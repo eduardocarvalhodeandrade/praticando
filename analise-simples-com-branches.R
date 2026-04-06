ae085e959ed004e593fd77f5ec6f47b868fd991a
= <=>
2026-04-06 10:22:05 -0300

    Adicionados análise de frequencias e gráficos

--- analise-simples-com-branches.R
+++ analise-simples-com-branches.R
@@ -8,15 +8,9 @@ dadoslimpos$faixa_etaria = ifelse(dadoslimpos$idade < 30, "Jovem",
                             ifelse(dadoslimpos$idade < 60, "Adulto", "Idoso"))
 dadoslimpos$faixa_etaria = factor(dadoslimpos$faixa_etaria)
 
-# 2. Distribuição de frequências e gráficos
-
-# (PREENCHER NA BRANCH analise-graficos,)
-
-# use comandos para distribuição de frequências de sexo, gráfico de colunas para faixa_etaria por sexo, histograma de altura, boxplot de IMC por sexo
-
-#3. Medidas descritivas
-
-# (PREENCHER NA BRANCH analise-medidas)
-
-# use comandos de cálculo de mínimo, máximo, média, mediana e desvio-padrão para idade, altura, peso e IMC
+#table(dados$sexo)
+barplot(table(dadoslimpos$faixa_etaria, dadoslimpos$sexo), beside = TRUE, legend
+        = TRUE)
+hist(dadoslimpos$altura)
+boxplot(dadoslimpos$imc ~ dadoslimpos$sexo)
 
